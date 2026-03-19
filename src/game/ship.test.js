10d75a66ebc2c7002f59993d4ab177e7dc7394df
Miles Rigby <milesjamesrigby@gmail.com>
2026-03-19 10:21:54 +0000

    change Ship test assertion to require type Object
    The Ship factory function now needs to return an Object, not any value,
    as asserted in ship.test.js

--- src/game/ship.test.js
+++ src/game/ship.test.js
@@ -3,6 +3,8 @@ import Ship from './ship.js';
 describe('Ship game object', () => {
 
     // Varying ship sizes for different test cases
+    // Restriction: adjacent values should never be equal to ensure subsequent tests of the
+    // same feature (to ensure all valid inputs are handled) do not use the same values
     const shipSizes = [1, 3, 5, 2, 3, 4, 5, 2, 3, 2, 4, 2]
     let shipIndex = 0;
 
@@ -18,8 +20,35 @@ describe('Ship game object', () => {
         shipIndex++;
     });
 
-    it('exists', () => {
-        expect(ship).toBeDefined();
+    it('is an object', () => {
+        expect(ship).toEqual(expect.any(Object));
     });
+
+    /*
+    it ('has a hit() function', () => {
+        expect(ship.hit).toBeDefined();
+    });
+
+    /*
+    it ('', () => {
+
+    });
+
+    it ('', () => {
+
+    });
+
+    it ('', () => {
+
+    });
+
+    it ('', () => {
+
+    });
+
+    it ('', () => {
+
+    });
+    */
 
 });
