cebb397b9aa8e432bb6fabb0abb9585b4e51727a
Miles Rigby <milesjamesrigby@gmail.com>
2026-03-19 10:52:57 +0000

    Remove unnecessary rotating ship size from tests
    The array containing various ship sizes for tests which didn't require
    specific ship sizes was unclear and has been replaced with a single
    constant value. Tests that require their own specific size can
    construct their own ship objects.

--- src/game/ship.test.js
+++ src/game/ship.test.js
@@ -2,24 +2,15 @@ import Ship from './ship.js';
 
 describe('Ship game object', () => {
 
-    // Varying ship sizes for different test cases
-    // Restriction: adjacent values should never be equal to ensure subsequent tests of the
-    // same feature (to ensure all valid inputs are handled) do not use the same values
-    // Also should not contain ones, to allow checking that one action does not sink the ship
-    const shipSizes = [4, 3, 5, 2, 3, 4, 5, 2, 3, 2, 4, 2]
-    let shipIndex = 0;
+    // An arbitrary ship size for tests that require a ship object but don't care how big it is
+    const ARBITRARY_SIZE = 3
 
     // Test objects 
-    let shipSize;
     let ship;
 
     // Construct a new ship instance for each test, keeping track of the ship's size
     beforeEach(() => {
-        shipSize = shipSizes[shipIndex];
-        ship = Ship(shipSize);
-
-        shipIndex++;
-        if (shipIndex === shipSizes.length) {shipIndex = 0;}
+        ship = Ship(ARBITRARY_SIZE);
     });
 
     it('is an object', () => {
