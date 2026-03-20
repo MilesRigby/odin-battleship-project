75a291c25bb5eaac5514d30e2f8466068f23ef16
Miles Rigby <milesjamesrigby@gmail.com>
2026-03-20 09:42:56 +0000

    Alter gameBoard.test.js to use nested describes
    Each function in the GameBoard factory function will now have its own describe block
    under the main describe for the factory itself for better readability.
    Wording of nested it statements altered to reflect the funciton they describe rather
    than a behaviour of the game board objects

--- src/game/gameBoard.test.js
+++ src/game/gameBoard.test.js
@@ -21,17 +21,6 @@ describe('Game Board object', () => {
         expect(gameBoard.receiveAttack).toEqual(expect.any(Function));
     });
 
-    it('returns an array from getBoardState', () => {
-        expect(gameBoard.getBoardState()).toEqual(expect.any(Array));
-    });
-
-    it('returns a 10*10 grid from getBoardState', () => {
-        const state = gameBoard.getBoardState();
-        expect(state[0][9]).toEqual(expect.anything());
-        expect(state[5][9]).toEqual(expect.anything());
-        expect(state[9][9]).toEqual(expect.anything());
-    });
-
     it('initialises game board as empty', () => {
         const state = gameBoard.getBoardState();
         expect(state[0][9]).toBe('empty');
@@ -39,35 +28,54 @@ describe('Game Board object', () => {
         expect(state[9][9]).toBe('empty');
     });
 
-    it('registers misses with receiveAttack on empty spaces', () => {
-        gameBoard.receiveAttack({x: 4, y: 7});
-        gameBoard.receiveAttack({x: 9, y: 2});
-        state = gameBoard.getBoardState();
+    describe('getBoardState()', () => {
 
-        expect(state[4][7]).toBe('miss');
-        expect(state[9][2]).toBe('miss');
+        it('returns an array', () => {
+            expect(gameBoard.getBoardState()).toEqual(expect.any(Array));
+        });
+
+        it('returns a 10*10 grid', () => {
+            const state = gameBoard.getBoardState();
+            expect(state[0][9]).toEqual(expect.anything());
+            expect(state[5][9]).toEqual(expect.anything());
+            expect(state[9][9]).toEqual(expect.anything());
+        });
+
     });
 
-    it('does nothing to empty spaces that are not targeted in receiveAttack', () => {
-        gameBoard.receiveAttack({x: 2, y: 5});
-        gameBoard.receiveAttack({x: 7, y: 2});
-        state = gameBoard.getBoardState();
+    describe('receiveAttack()', () => {
 
-        expect(state[4][9]).toBe('empty');
-        expect(state[6][2]).toBe('empty');
-    });
+        it('changes "empty" spaces to "miss" when targeted', () => {
+            gameBoard.receiveAttack({x: 4, y: 7});
+            gameBoard.receiveAttack({x: 9, y: 2});
+            state = gameBoard.getBoardState();
 
-    it('returns true from receiveAttack when targeting an empty space', () => {
-        expect(gameBoard.receiveAttack({x: 1, y: 0})).toBe(true);
-        expect(gameBoard.receiveAttack({x: 8, y: 9})).toBe(true);
-    });
+            expect(state[4][7]).toBe('miss');
+            expect(state[9][2]).toBe('miss');
+        });
 
-    it('returns false from receiveAttack when targeting an already missed space', () => {
-        gameBoard.receiveAttack({x: 7, y: 4});
-        gameBoard.receiveAttack({x: 3, y: 2});
+        it('does nothing to untargeted empty spaces', () => {
+            gameBoard.receiveAttack({x: 2, y: 5});
+            gameBoard.receiveAttack({x: 7, y: 2});
+            state = gameBoard.getBoardState();
 
-        expect(gameBoard.receiveAttack({x: 7, y: 4})).toBe(false);
-        expect(gameBoard.receiveAttack({x: 3, y: 2})).toBe(false);
+            expect(state[4][9]).toBe('empty');
+            expect(state[6][2]).toBe('empty');
+        });
+
+        it('returns true when targeting an empty space', () => {
+            expect(gameBoard.receiveAttack({x: 1, y: 0})).toBe(true);
+            expect(gameBoard.receiveAttack({x: 8, y: 9})).toBe(true);
+        });
+
+        it('returns false when targeting a previously missed space', () => {
+            gameBoard.receiveAttack({x: 7, y: 4});
+            gameBoard.receiveAttack({x: 3, y: 2});
+
+            expect(gameBoard.receiveAttack({x: 7, y: 4})).toBe(false);
+            expect(gameBoard.receiveAttack({x: 3, y: 2})).toBe(false);
+        });
+        
     });
 
 });
