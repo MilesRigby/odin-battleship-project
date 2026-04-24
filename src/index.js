a8e2e1565f4f4f0fd414b8fe2d5dc5b4074e3329
Miles Rigby <milesjamesrigby@gmail.com>
2026-04-24 15:35:28 +0100

    remove game loop and UI related code, plus CSS
    As the game events loop and UI were heavily interdependant, I have
    chosen to recreate these components, ustilising more effective SOLID principles.
    I also plan on rewriting the CSS across multiple files for the final prokject rather than a single one.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
 import "./styles.css";
-import pageConstructor from './UI/pageContent.js';
-import GameEvents from "./Events/gameEvents.js";
-
-const page = pageConstructor();
-
-document.body.appendChild(page.content.pageContent);
-
-const gameEvents = GameEvents(page);
-
-gameEvents.setup(await new Promise((resolve) => {
-    page.temps.getPlayerTypes(resolve);
-}));
