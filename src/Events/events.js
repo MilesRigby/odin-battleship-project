fb33d22d8e903ad34837bb8b6668b13b98c7f00c
Miles Rigby <milesjamesrigby@gmail.com>
2026-04-29 14:56:39 +0100

    add basic comments to the events system code

--- src/Events/events.js
+++ src/Events/events.js
@@ -1,12 +1,17 @@
 const eventsConstructor = () => {
 
+    // Initialise events with an empty object
     const events = {};
 
+    // Adds the event name as a key in the above object if it doesn't already exist; 
+    // adds the listener's callback to the array that key has as its value
     const listen = (name, fn) => { 
         if ( !events[name] ) events[name] = [];
         events[name].push(fn);
     }
 
+    // Calls every callback registered to the event [name];
+    // passes a data object as the lone parameter to every callback
     const emit = (name, data) => { 
         fnArr = events[name];
 
@@ -17,15 +22,15 @@ const eventsConstructor = () => {
         }
     }
 
+    // only listening and emitting actions are accessible externally, not the events object
     return {
         listen: listen,
         emit: emit        
     }
 }
 
-const events = eventsConstructor();
-
 // single instance exported for use in project
+const events = eventsConstructor();
 export default events;
 
 // constructor exported for testing with Jest
