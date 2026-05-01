b973d5a721cb58da374cdcea2018f293ea8db039
Miles Rigby <milesjamesrigby@gmail.com>
2026-05-01 17:31:01 +0100

    add setup of CPU gameboards to game logic
    the tests used for this behave very strangely.
    If board_state_changed sends a board state that doesn't match the expected value,
    the entire test suite crashes.
    
    This is unless another test in the suite also fails, in which case it works fine,
    though is still potentially difficult to read.

--- src/Events/GameLogic.js
+++ src/Events/GameLogic.js
@@ -1,17 +1,34 @@
 import eventsSys from './events.js';
+import Player from '../Objects/player.js';
 
 const GameLogic = (events = eventsSys) => {
 
     events.listen('player_types_selected', ({playerOneType, playerTwoType}) => {
-        if (playerOneType !== 'real') events.emit('board_state_changed', {board: 1});
-        if (playerTwoType !== 'real') events.emit('board_state_changed', {board: 2});
+        const playerOne = Player(playerOneType);
+        const playerTwo = Player(playerTwoType);
 
-        if (playerOneType === 'real' && playerTwoType === 'real') {
+        if (playerOne.type !== 'real') {
+            _populateGameBoard(playerOne);
+            events.emit('board_state_changed', {board: 1, state: playerOne.board.getBoardState()});
+        }
+        if (playerTwo.type !== 'real') {
+            _populateGameBoard(playerTwo);
+            events.emit('board_state_changed', {board: 2, state: playerTwo.board.getBoardState()});
+        }
+
+        if (playerOne.type === 'real' && playerTwo.type === 'real') {
             events.emit('player_ship_placement_required', {player: 'Player 1', length: 0});
-        } else if (playerOneType === 'real' || playerTwoType === 'real') {
+        } else if (playerOne.type === 'real' || playerTwo.type === 'real') {
             events.emit('player_ship_placement_required', {player: '', length: 0});
         }
     });
+
+    const _populateGameBoard = (player) => {
+        const shipLengths = [2, 3, 3, 4, 5];
+        for ( const length of shipLengths ) {
+            player.board.addShip({x: Math.floor(Math.random()*10), y: Math.floor(Math.random()*10)}, length, Math.floor(Math.random()*4));
+        }
+    }
 
     events.emit('game_started');
 
--- src/Events/GameLogic.test.js
+++ src/Events/GameLogic.test.js
@@ -31,11 +31,13 @@ describe('Game logic handler', () => {
             GameLogic(events);
         });
 
+
+
         describe('on event:player_types_selected', () => {
 
             // Order in which the ship lengths 2, 3, 3, 4, and 5 are asked for is an implementation detail
             describe('emits event:player_ship_placement_required if one or both players are human, specifying a ship length and the first player if there are two humans', () => {
-                it.each([
+                test.each([
                     ['real', 'computer', ''],
                     ['computer', 'real', ''],
                     ['real', 'real', 'Player 1']
@@ -58,7 +60,7 @@ describe('Game logic handler', () => {
             });
 
             describe('emits event:board_state_changed a number of times equal to the number of CPUs', () => {
-                it.each([
+                test.each([
                     ['real', 'real', 0],
                     ['real', 'computer', 1],
                     ['computer', 'computer', 2]
@@ -71,8 +73,8 @@ describe('Game logic handler', () => {
                 });
             });
 
-            describe('emits event:board_state_changed, conveying which board was changed', () => {
-                it('real, computer', () => {
+            describe('emits event:board_state_changed for CPUs, conveying which board was changed', () => {
+                test('real, computer', () => {
                     const MockCallback = jest.fn();
                     events.listen('board_state_changed', MockCallback);
 
@@ -80,17 +82,133 @@ describe('Game logic handler', () => {
                     expect(MockCallback).toHaveBeenCalledWith(expect.objectContaining({board: 2}));
                 });
 
-                it('computer, computer', () => {
+                test('computer, computer', () => {
                     const MockCallback = jest.fn();
                     events.listen('board_state_changed', MockCallback);
 
                     events.emit('player_types_selected', {playerOneType: 'computer', playerTwoType: 'computer'});
-                    expect(MockCallback.mock.calls[0]).toEqual([ expect.objectContaining({board: 1}) ]);
-                    expect(MockCallback.mock.calls[1]).toEqual([ expect.objectContaining({board: 2}) ]);
+                    expect(MockCallback.mock.calls[0][0]).toEqual(expect.objectContaining({board: 1}));
+                    expect(MockCallback.mock.calls[1][0]).toEqual(expect.objectContaining({board: 2}));
+                });
+            });
+
+            // These tests are large, difficult to read diffs for, and crash the entire test suite unless another test in the suite also fails (no idea)
+            // I unfortunately don't know a better way of doing this.
+            describe('emits event:board_state_changed with a 10*10 board state produced using randomness', () => {
+                test('case 1', () => {
+                    const MockCallback = jest.fn();
+                    events.listen('board_state_changed', MockCallback);
+
+                    const boardOneExpected = [
+                        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+                        [ 0, 1, 0, 2, 0, 0, 0, 0, 0, 0 ],
+                        [ 0, 1, 0, 2, 0, 0, 3, 0, 0, 0 ],
+                        [ 0, 0, 0, 2, 0, 0, 3, 0, 0, 0 ],
+                        [ 0, 0, 5, 0, 0, 0, 3, 0, 0, 0 ],
+                        [ 0, 0, 5, 0, 0, 0, 0, 0, 0, 0 ],
+                        [ 0, 0, 5, 0, 0, 4, 4, 4, 4, 0 ],
+                        [ 0, 0, 5, 0, 0, 0, 0, 0, 0, 0 ],
+                        [ 0, 0, 5, 0, 0, 0, 0, 0, 0, 0 ],
+                        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+                    ];
+
+                    const boardTwoExpected = [
+                        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+                        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+                        [ 0, 0, 4, 4, 4, 4, 0, 0, 5, 0 ],
+                        [ 0, 0, 0, 0, 0, 0, 0, 0, 5, 0 ],
+                        [ 0, 0, 0, 1, 1, 0, 2, 0, 5, 0 ],
+                        [ 0, 0, 0, 0, 0, 0, 2, 0, 5, 0 ],
+                        [ 0, 0, 0, 0, 0, 0, 2, 0, 5, 0 ],
+                        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+                        [ 0, 0, 0, 0, 3, 3, 3, 0, 0, 0 ],
+                        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+                    ];
+
+                    // for 10 ships - x, y (0-9), and orientation (0-3)
+                    // Math.random gives 0-1, so 0-9 is 0.05-0.95, and 0-3 is 0.2-0.95, each in equal increments.
+                    const deterministicValues = [
+                        0.15, 0.15, 0.45,
+                        0.15, 0.35, 0.45,
+                        0.25, 0.65, 0.45,
+                        0.65, 0.55, 0.2,
+                        0.85, 0.25, 0.95,
+                        0.45, 0.35, 0.2,
+                        0.45, 0.65, 0.45,
+                        0.85, 0.65, 0.7,
+                        0.25, 0.25, 0.2,
+                        0.25, 0.85, 0.45
+                    ];
+
+                    let v = 0;
+                    jest.spyOn(Math, 'random').mockImplementation(() => deterministicValues[v++]);
+
+                    events.emit('player_types_selected', {playerOneType: 'computer', playerTwoType: 'computer'});
+                    expect(MockCallback.mock.calls[0][0].state).toEqual(boardOneExpected);
+                    expect(MockCallback.mock.calls[1][0].state).toEqual(boardTwoExpected);
+                });
+
+                test('case 2', () => {
+                    const MockCallback = jest.fn();
+                    events.listen('board_state_changed', MockCallback);
+
+                    const boardOneExpected = [
+                        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+                        [ 0, 0, 0, 0, 2, 0, 0, 0, 0, 0 ],
+                        [ 0, 1, 0, 0, 2, 0, 3, 3, 3, 0 ],
+                        [ 0, 1, 0, 0, 2, 0, 0, 0, 0, 0 ],
+                        [ 0, 0, 0, 5, 0, 0, 0, 0, 0, 0 ],
+                        [ 0, 0, 0, 5, 0, 0, 0, 0, 0, 0 ],
+                        [ 0, 0, 0, 5, 4, 4, 4, 4, 0, 0 ],
+                        [ 0, 0, 0, 5, 0, 0, 0, 0, 0, 0 ],
+                        [ 0, 0, 0, 5, 0, 0, 0, 0, 0, 0 ],
+                        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+                    ];
+
+                    const boardTwoExpected = [
+                        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+                        [ 0, 0, 4, 4, 4, 4, 0, 0, 0, 0 ],
+                        [ 0, 0, 0, 0, 0, 0, 0, 5, 0, 0 ],
+                        [ 0, 0, 0, 0, 0, 0, 2, 5, 0, 0 ],
+                        [ 0, 0, 1, 1, 0, 0, 2, 5, 0, 0 ],
+                        [ 0, 0, 0, 0, 0, 0, 2, 5, 0, 0 ],
+                        [ 0, 0, 0, 0, 0, 0, 0, 5, 0, 0 ],
+                        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+                        [ 0, 0, 0, 0, 3, 3, 3, 0, 0, 0 ],
+                        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+                    ];
+
+                    // for 10 ships - x, y (0-9), and orientation (0-3)
+                    // Math.random gives 0-1, so 0-9 is 0.05-0.95, and 0-3 is 0.2-0.95, each in equal increments.
+                    const deterministicValues = [
+                        0.25, 0.15, 0.45,
+                        0.15, 0.45, 0.45,
+                        0.25, 0.65, 0.2,
+                        0.65, 0.45, 0.2,
+                        0.85, 0.35, 0.95,
+                        0.45, 0.35, 0.7,
+                        0.35, 0.65, 0.45,
+                        0.85, 0.65, 0.7,
+                        0.15, 0.25, 0.2,
+                        0.25, 0.75, 0.45
+                    ];
+
+                    let v = 0;
+                    jest.spyOn(Math, 'random').mockImplementation(() => deterministicValues[v++])
+
+                    events.emit('player_types_selected', {playerOneType: 'computer', playerTwoType: 'computer'});
+                    expect(MockCallback.mock.calls[0][0].state).toEqual(boardOneExpected);
+                    expect(MockCallback.mock.calls[1][0].state).toEqual(boardTwoExpected);
                 });
             });
 
         });
+
+
+
+        //
+
+
 
     });
 
