a273fe1df8a3b2e96d841f7846a5053167297d53
Miles Rigby <milesjamesrigby@gmail.com>
2026-05-01 09:58:10 +0100

    make game logic request a human player's ship pos
    when a user selects the type (human or CPU) for each player, the
    game logic must send a signal stating the position of a human player's
    first ship is required, if any human players are present.
    This begins the process of adding ships to the board.

--- src/Events/GameLogic.js
+++ src/Events/GameLogic.js
@@ -2,6 +2,14 @@ import eventsSys from './events.js';
 
 const GameLogic = (events = eventsSys) => {
 
+    events.listen('player_types_selected', ({playerOneType, playerTwoType}) => {
+        if (playerOneType === 'real' && playerTwoType === 'real') {
+            events.emit('player_ship_placement_required', {player: 'Player 1', length: 0});
+        } else {
+            events.emit('player_ship_placement_required', {player: '', length: 0});
+        }
+    });
+
     events.emit('game_started');
 
 }
--- src/Events/GameLogic.test.js
+++ src/Events/GameLogic.test.js
@@ -24,18 +24,33 @@ describe('Game logic handler', () => {
 
     });
 
-    describe('behaviour when initialised, and of features not dependant on state', () => {
+    describe('behaviour during runtime', () => {
 
-        /*beforeEach(() => {
+        beforeEach(() => {
             events = eventsConstructor();
             GameLogic(events);
         });
 
-        it()*/
+        describe('on event:player_types_selected', () => {
 
-    });
+            // Order in which the ship lengths 2, 3, 3, 4, and 5 are asked for is an implementation detail
+            describe('emits event:player_ship_placement_required if one or both players are human, specifying a ship length and the first player if there are two humans', () => {
 
-    describe('behaviour during runtime (features dependant on a non-initial state)', () => {
+                it.each([
+                    ['real', 'computer', ''],
+                    ['computer', 'real', ''],
+                    ['real', 'real', 'Player 1']
+                ])('%s, %s', (p1, p2, o1) => {
+                    const MockCallback = jest.fn();
+                    events.listen('player_ship_placement_required', MockCallback);
+
+                    events.emit('player_types_selected', {playerOneType: p1, playerTwoType: p2});
+                    expect(MockCallback).toHaveBeenCalledWith(expect.objectContaining({player: o1, length: expect.any(Number)}));
+                });
+
+            });
+
+        });
 
     });
 
